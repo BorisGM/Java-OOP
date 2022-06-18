5837be16a5cfb88ec64070f8f28584309fb0baef
BorisGM <93388190+BorisGM@users.noreply.github.com>
2022-06-18 20:37:33 +0300

    ex

---
+++ SoulMaster.java
@@ -0,0 +1,8 @@
+import hero.DarkWizard;
+
+//created by B.M.
+public class SoulMaster extends DarkWizard {
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
