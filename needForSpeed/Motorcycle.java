92aec56285e8bf3cddaf6e99f99c5d2144448317
BorisGM <93388190+BorisGM@users.noreply.github.com>
2022-06-21 22:19:38 +0300

    ex

---
+++ needForSpeed/Motorcycle.java
@@ -0,0 +1,9 @@
+package needForSpeed;
+//created by B.M.
+
+public class Motorcycle extends Vehicle{
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
