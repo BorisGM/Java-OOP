529f58a1c5d0da7a2fc54a3b9b5e9a647f6a9d49
BorisGM <93388190+BorisGM@users.noreply.github.com>
2022-09-13 20:51:06 +0300

    ex

--- UnitTesting/Exercises/src/main/java/p05_CustomLinkedList/CustomLinkedList.java
+++ UnitTesting/Exercises/src/main/java/p05_CustomLinkedList/CustomLinkedList.java
@@ -19,5 +19,16 @@ public class CustomLinkedList<T> {
         public T getElement() {
             return element;
         }
+        public void setElement(T element) {
+            this.element = element;
+        }
+
+        public ListNode getNextNode() {
+            return nextNode;
+        }
+
+        public void setNextNode(ListNode nextNode) {
+            this.nextNode = nextNode;
+        }
     }
 }
